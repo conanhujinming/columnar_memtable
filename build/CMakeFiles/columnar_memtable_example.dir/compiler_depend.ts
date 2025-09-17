10b5c434619af86138f798ee3dc8e0f8d54c9191
Jinming Hu <438600480@qq.com>
2025-09-17 12:27:38 +0800

    remove build

--- build/CMakeFiles/columnar_memtable_example.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for columnar_memtable_example.
--- build/CMakeFiles/columnar_memtable_example.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for columnar_memtable_example.
-# This may be replaced when dependencies are built.
